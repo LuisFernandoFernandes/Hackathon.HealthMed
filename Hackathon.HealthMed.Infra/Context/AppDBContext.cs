0238e34c400c272e43177f07d3535860a6d8806c
Luís Fernando Fernandes <luisfernando.fernandes@hotmail.com>
2025-03-08 18:42:11 -0300

    feat: criação do AppDBContext

---
+++ Hackathon.HealthMed.Infra/Context/AppDBContext.cs
@@ -0,0 +1,36 @@
+﻿using Hackathon.HealthMed.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Hackathon.HealthMed.Infra.Context;
+
+public class AppDBContext : DbContext
+{
+    public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDBContext).Assembly);
+    }
+
+    public DbSet<Agendamento> Agendamentos { get; set; }
+    public DbSet<Horario> Horarios { get; set; }
+    public DbSet<Medico> Medicos { get; set; }
+    public DbSet<Paciente> Pacientes { get; set; }
+    public DbSet<Usuario> Usuarios { get; set; }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCriacao") != null))
+        {
+            if (entry.State == EntityState.Modified)
+            {
+                entry.Property("DataCriacao").IsModified = false;
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
+}
