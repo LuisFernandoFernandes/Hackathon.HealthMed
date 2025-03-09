33caa2d9b44229ae7606f2860010b0d9c4d2bf5c
Luís Fernando Fernandes <luisfernando.fernandes@hotmail.com>
2025-03-09 16:41:38 -0300

    feat: criação do ServiceResult

---
+++ Hackathon.HealthMed.Application/Result/ServiceResult.cs
@@ -0,0 +1,18 @@
+﻿namespace Hackathon.HealthMed.Application.Result;
+public class ServiceResult<T>
+{
+    public T? Data { get; }
+    public Exception? Error { get; }
+    public bool IsSuccess => Error == null;
+    public ServiceResult(T data)
+    {
+        Data = data;
+        Error = null;
+    }
+
+    public ServiceResult(Exception error)
+    {
+        Data = default;
+        Error = error;
+    }
+}
