c5230748c189771c3251cec62e827d6effefa606
Luís Fernando Fernandes <luisfernando.fernandes@hotmail.com>
2025-03-09 17:27:29 -0300

    feat: criação de migration inicial

--- Hackathon.HealthMed.Infra/Mappings/AgendamentoConfiguration.cs
+++ Hackathon.HealthMed.Infra/Mappings/AgendamentoConfiguration.cs
@@ -10,9 +10,9 @@ public class AgendamentoConfiguration : IEntityTypeConfiguration<Agendamento>
         builder.ToTable("Agendamentos");
         builder.HasKey(a => a.Id);
         builder.Property(a => a.PacienteId).IsRequired();
-        builder.HasOne(a => a.Paciente).WithMany().HasForeignKey(a => a.PacienteId).IsRequired();
+        builder.HasOne(a => a.Paciente).WithMany().HasForeignKey(a => a.PacienteId).OnDelete(DeleteBehavior.NoAction).IsRequired();
         builder.Property(a => a.HorarioId).IsRequired();
-        builder.HasOne(a => a.Horario).WithOne().HasForeignKey<Agendamento>(a => a.HorarioId).IsRequired();
+        builder.HasOne(a => a.Horario).WithOne().HasForeignKey<Agendamento>(a => a.HorarioId).OnDelete(DeleteBehavior.NoAction).IsRequired();
         builder.Property(a => a.Status).IsRequired();
         builder.Property(a => a.JustificativaCancelamento).HasMaxLength(255).IsRequired(false);
         builder.Property(c => c.DataCriacao).HasColumnType("smalldatetime");
---
+++ Hackathon.HealthMed.Infra/Migrations/20250309202656_InitialCreate.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hackathon.HealthMed.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: true),
+                    SenhaHash = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    TipoUsuario = table.Column<int>(type: "int", nullable: false),
+                    Ativo = table.Column<bool>(type: "bit", nullable: false),
+                    DataCriacao = table.Column<DateTime>(type: "smalldatetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Medicos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UsuarioId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CRM = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Especialidade = table.Column<int>(type: "int", nullable: false),
+                    DataCriacao = table.Column<DateTime>(type: "smalldatetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medicos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Medicos_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pacientes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UsuarioId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Cpf = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    DataCriacao = table.Column<DateTime>(type: "smalldatetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pacientes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pacientes_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Horarios",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MedicoId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DataHorario = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    Valor = table.Column<decimal>(type: "decimal(10,2)", nullable: false),
+                    DataCriacao = table.Column<DateTime>(type: "smalldatetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Horarios", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Horarios_Medicos_MedicoId",
+                        column: x => x.MedicoId,
+                        principalTable: "Medicos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Agendamentos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PacienteId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    HorarioId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    JustificativaCancelamento = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    DataCriacao = table.Column<DateTime>(type: "smalldatetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Agendamentos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Agendamentos_Horarios_HorarioId",
+                        column: x => x.HorarioId,
+                        principalTable: "Horarios",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Agendamentos_Pacientes_PacienteId",
+                        column: x => x.PacienteId,
+                        principalTable: "Pacientes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Agendamentos_HorarioId",
+                table: "Agendamentos",
+                column: "HorarioId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Agendamentos_PacienteId",
+                table: "Agendamentos",
+                column: "PacienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Horarios_MedicoId",
+                table: "Horarios",
+                column: "MedicoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Medicos_UsuarioId",
+                table: "Medicos",
+                column: "UsuarioId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pacientes_UsuarioId",
+                table: "Pacientes",
+                column: "UsuarioId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Agendamentos");
+
+            migrationBuilder.DropTable(
+                name: "Horarios");
+
+            migrationBuilder.DropTable(
+                name: "Pacientes");
+
+            migrationBuilder.DropTable(
+                name: "Medicos");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
