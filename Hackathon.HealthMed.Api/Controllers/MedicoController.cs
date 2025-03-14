a9e1a8c71b355443dc7cec8e209a3f2ccce2b425
Luís Fernando Fernandes <luisfernando.fernandes@hotmail.com>
2025-03-13 22:08:44 -0300

    feat: inclusão de autorização na controller de médicos e autorização por role de paciente para buscar médicos filtrando ou não por especialidades

--- Hackathon.HealthMed.Api/Controllers/MedicoController.cs
+++ Hackathon.HealthMed.Api/Controllers/MedicoController.cs
@@ -1,14 +1,17 @@
 ﻿using Hackathon.HealthMed.Application.Interfaces;
 using Hackathon.HealthMed.Domain.Enums;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Hackathon.HealthMed.Api.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
+[Authorize]
 public class MedicoController(IMedicoService medicoService) : ControllerBase
 {
     [HttpGet]
+    [Authorize(Roles = "Paciente")]
     public async Task<IActionResult> GetMedicos([FromQuery] eEspecialidade? especialidade)
     {
         var resultado = await medicoService.BuscarMedicos(especialidade);
